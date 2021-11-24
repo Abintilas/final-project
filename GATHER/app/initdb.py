77893b47487875b4d1548f62ea4f8b3800a97edf
Abintilas <69883318+Abintilas@users.noreply.github.com>
2021-11-24 18:53:46 +0530

    Update initdb.py
    Adding extra layer protection

--- GATHER/app/initdb.py
+++ GATHER/app/initdb.py
@@ -1,22 +1,24 @@
 from hashlib import md5
-
+from passlib.context import CryptContext 
 from app.database import db
 from app.models import User
 
- 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")  
 def create_admin():
 
+    
     user = User.query.filter_by(email="admin@site.net").first()
 
     if user is None:
         
         user = User(
             username="admin",
-            password=md5("password".encode()).hexdigest(),
+            password=pwd_context.hash("passwords"),
             email="admin@site.net",
             is_admin=True
         )
 
+        
         db.session.add(user)
         db.session.commit()
         print("super user admin set successfully")
